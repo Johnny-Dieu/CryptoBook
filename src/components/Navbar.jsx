385bd29baa9e95c6917fd22966f0a3bc4201e414
Johnny-Dieu <46733028+Johnny-Dieu@users.noreply.github.com>
2021-12-18 00:29:04 -0500

    site improvements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     }, []);
 
     useEffect(() => {
-        if(screenSize < 768){
+        if(screenSize <= 900){
             setActiveMenu(false);
         }
         else{
@@ -30,27 +30,27 @@ const Navbar = () => {
     return (
         <div className="nav-container">
             <div className="logo-container">
-                <Avatar src={icon} className="avatar"/>
-                <Typography.Title level={1} className="logo">
+                <Avatar src={icon} size={70}/>
+                <Typography.Title level={2} className="logo">
                     <Link to="/">CryptoBook</Link>
                 </Typography.Title>
+            </div>
                 <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
                     <MenuOutlined/>
                 </Button>
-            </div>
             {activeMenu &&(
                 <Menu selectedKeys="/" style={{color: "red"}}>
                 <Menu.Item icon={<HomeOutlined className="menu-item-icon"/>} key="1">
-                    <NavLink to="/" style={{ color: '#FFF' }}>Home</NavLink>
+                    <NavLink to="/" style={{ color: '#FFF' }} onClick={() => setActiveMenu(!activeMenu)}>Home</NavLink>
                 </Menu.Item>
                 <Menu.Item icon={<FundOutlined className="menu-item-icon"/>} key="2">
-                    <NavLink to="/cryptocurrencies" style={{ color: '#FFF' }}>Crypto Currencies</NavLink>
+                    <NavLink to="/cryptocurrencies" style={{ color: '#FFF' }} onClick={() => setActiveMenu(!activeMenu)}>Crypto Currencies</NavLink>
                 </Menu.Item>
                 <Menu.Item icon={<MoneyCollectOutlined className="menu-item-icon"/>} key="3">
-                    <NavLink to="/exchanges" style={{ color: '#FFF' }}>Exchanges</NavLink>
+                    <NavLink to="/exchanges" style={{ color: '#FFF' }} onClick={() => setActiveMenu(!activeMenu)}>Exchanges</NavLink>
                 </Menu.Item>
                 <Menu.Item icon={<BulbOutlined className="menu-item-icon"/>} key="4">
-                    <NavLink to="/news" style={{ color: '#FFF' }}>News</NavLink>
+                    <NavLink to="/news" style={{ color: '#FFF' }} onClick={() => setActiveMenu(!activeMenu)}>News</NavLink>
                 </Menu.Item>
             </Menu>
             )}
